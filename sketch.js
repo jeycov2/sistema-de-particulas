1b1f783380158e84f5b4b174d38fe756a9565630
Jacobo Narváez <jacobo.narvaez@ucuenca.edu.ec>
2022-11-23 16:44:33 -0600

    random walker solo print

--- sketch.js
+++ sketch.js
@@ -1,7 +1,42 @@
+let npelotas = 100
+let pelotas = []
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < npelotas; i++) {
+    pelotas[i] = new RandomWalker();
+  }
+
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+
 }
+
+
+
+
+//______Clases_______//
+//___________________//
+//___________________//
+
+//random walker//
+
+class RandomWalker {
+  constructor() {
+    this.posX = random(width);
+    this.posY = random(height);
+    this.speed = random(4);
+    this.diametro = random(10, 50);
+    print("hola");
+  }
+  update() {
+    this.posX += random(-this.speed, this.speed);
+    this.posY += random(-this.speed, this.speed);
+
+  }
+  display() {
+    ellipse(this.posX, this.posY, this.diametro, this.diametro);
+
+  }
+}
+
